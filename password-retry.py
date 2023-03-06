734ed5bcacc7ea75855926149750a29b6e3444bd
Ryder Tsai <kaoru0911@gmail.com>
2023-03-07 04:28:01 +0800

    better

--- password-retry.py
+++ password-retry.py
@@ -1,13 +1,17 @@
 password = 'a123456'
 time = 3
 while time > 0 :
+	time = time - 1
 	enter = input('請輸入密碼：')
 	if enter == password :
 		print('登入成功')
-		break #逃出迴圈
+		break
 	else:
-		print('密碼錯誤，還有', time - 1, '次機會')
-		time = time - 1
+		print('密碼錯誤')
+		if time > 0:
+			print('還有', time, '次機會')
+		else:
+			print('密碼錯誤三次，帳號已鎖定')
 
 
 
